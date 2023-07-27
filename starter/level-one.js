14e4c2489bec31e9aa6d4c244fbe4e4a5cefe95c
Trishaneww <92243834+Trishaneww@users.noreply.github.com>
2023-07-27 18:44:11 -0400

    updated score

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,17 @@
  * DOTS: Level One
  *
  */
+
+let score = 0;
+const ball = document.querySelector('.js-ball');
+
+const scoreDisplay = document.querySelector('.js-score');
+const levelWinner = document.querySelector('.level-winner');
+
+ball.addEventListener('click', function() {
+    score=score+5;
+    scoreDisplay.innerText = score;
+    if (score>=100) {
+        levelWinner.style.opacity = 1;
+    }
+})
